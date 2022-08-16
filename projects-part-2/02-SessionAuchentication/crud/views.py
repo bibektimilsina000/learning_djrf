95096f7060e17e41355f46d4feb73a98e96b825c
cicero <bibektimilsina000@gmail.com>
2022-08-16 15:20:02 +0545

    session auth completed

--- projects-part-2/02-SessionAuchentication/crud/views.py
+++ projects-part-2/02-SessionAuchentication/crud/views.py
@@ -1,14 +1,14 @@
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import viewsets
-from rest_framework.authentication import BasicAuthentication
+from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    authentication_classes = [BasicAuthentication]
+    authentication_classes = [SessionAuthentication]
     permission_classes = [IsAuthenticated]
     # permission_classes = [AllowAny]
     # permission_classes = [IsAdminUser]
