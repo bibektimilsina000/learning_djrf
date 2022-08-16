7fe40f41f2f49c77c32316a4705786b65da02bb5
cicero <bibektimilsina000@gmail.com>
2022-08-16 12:47:53 +0545

    Basicauth

---
+++ projects-part-2/01-BasicAuthentication/crud/views.py
@@ -0,0 +1,14 @@
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    # permission_classes = [AllowAny]
+    # permission_classes = [IsAdminUser]
